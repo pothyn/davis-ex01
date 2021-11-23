7d5f13c635983c92b22bdd8cdbf7d412a73fa357
pothyn <pothyne@outlook.com>
2021-11-23 16:00:08 -0500

    removed other exercises

--- src/main/java/baseline/Solution01.java
+++ src/main/java/baseline/Solution01.java
@@ -2,16 +2,20 @@
  *  UCF COP3330 Fall 2021 Assignment 1 Solution
  *  Copyright 2021 Hunter Davis
  */
-import java.sql.SQLOutput;
+package baseline;
+
 import java.util.Scanner;
 
 public class Solution01 {
     public static void main(String[] args) {
+        // New scanner to take input
         Scanner in = new Scanner(System.in);
 
+        // Prompt and read input
         System.out.print("Enter your name: ");
         String str = in.nextLine();
 
+        // Print and return name
         System.out.println("Your name is " + str + ".");
     }
 }
